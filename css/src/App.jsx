0e8a0c7d44d2f60ca48d99997951272ca80f79b1
lakshmi-akshitha <lakshmiakshitha.y@gmail.com>
2026-06-15 22:16:35 +0530

    css

---
+++ css/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Button from './components/Button'
+import Header from './components/Header'
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <Button />
+    </>
+  )
+}
+
+export default App
---
+++ css/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styles/Header.css'
+const Header = () => {
+  return (
+    <div className='header'>
+     <h3>  Welcome </h3>
+      <button >Login</button>
+    </div>
+  )
+}
+
+export default Header
